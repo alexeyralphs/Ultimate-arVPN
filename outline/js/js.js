87a70f3941a38734d5639c993e71fad95f970812
alexeyralphs <138135367+alexeyralphs@users.noreply.github.com>
2025-02-06 23:04:03 +0300

    fixed

--- outline/js/js.js
+++ outline/js/js.js
@@ -32,7 +32,7 @@ function copyToClipboard(text) {
 
 function resetAuth() {
 	let xhr = new XMLHttpRequest();
-	xhr.open('GET', `https://invalid:invalid@${host}/`, true);
+	xhr.open('GET', `https://invalid:invalid@${host}/outline/`, true);
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState === 4) {
 			window.location.reload(true);
