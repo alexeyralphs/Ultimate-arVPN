ea447f476ce37c4bc8c85138f450b5e7a55da77f
alexeyralphs <138135367+alexeyralphs@users.noreply.github.com>
2025-02-08 01:47:25 +0300

    js

--- outline/js/js.js
+++ outline/js/js.js
@@ -30,17 +30,6 @@ function copyToClipboard(text) {
 	}
 }
 
-function resetAuth() {
-	let xhr = new XMLHttpRequest();
-	xhr.open('GET', `https://invalid:invalid@${host}/outline/`, true);
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState === 4) {
-			window.location.reload(true);
-		}
-	};
-	xhr.send();
-}
-
 async function createClientKey(name) {
 	return new Promise((resolve, reject) => {
 		let xhr = new XMLHttpRequest();
---
+++ outline/js/resetAuth.js
@@ -0,0 +1,10 @@
+function resetAuth() {
+	let xhr = new XMLHttpRequest();
+	xhr.open('GET', `https://invalid:invalid@${host}/outline/`, true);
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4) {
+			window.location.reload(true);
+		}
+	};
+	xhr.send();
+}
