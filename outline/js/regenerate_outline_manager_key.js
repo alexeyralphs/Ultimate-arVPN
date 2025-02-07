fc13c30c510709e80913962cc64fe37fe20bc6e6
alexeyralphs <138135367+alexeyralphs@users.noreply.github.com>
2025-02-08 01:12:35 +0300

    js

--- outline/js/regenerate_outline_manager_key.js
+++ outline/js/regenerate_outline_manager_key.js
@@ -1,4 +1,4 @@
-function createManagerKey() {
+/*function regenerate_outline_manager_key() {
 	let xhr = new XMLHttpRequest();
 	xhr.open('POST', './scripts/regenerate_outline_manager_key.php', true);
 	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
@@ -12,4 +12,25 @@ function createManagerKey() {
 		}
 	};
 	xhr.send();
+}*/
+
+function regenerate_outline_manager_key() {
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', './scripts/regenerate_outline_manager_key.php', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                let response = JSON.parse(xhr.responseText);
+                if (response.status === 'success') {
+                    console.log('Script executed successfully!');
+                } else {
+                    console.error('Error: ' + response.message);
+                }
+            } else {
+                console.error('Error: ' + xhr.statusText);
+            }
+        }
+    };
+    xhr.send();
 }
