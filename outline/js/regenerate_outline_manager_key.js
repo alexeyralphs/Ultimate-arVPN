f9f9f00fe49d718519794d3b2ca0c65d259daa6c
alexeyralphs <138135367+alexeyralphs@users.noreply.github.com>
2025-02-12 02:22:15 +0300

    js

--- outline/js/regenerate_outline_manager_key.js
+++ outline/js/regenerate_outline_manager_key.js
@@ -1,21 +1,26 @@
 function regenerate_outline_manager_key() {
     let xhr = new XMLHttpRequest();
-    xhr.open('POST', './scripts/regenerate_outline_manager_key.php', true);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.open('GET', './scripts/regenerate_outline_manager_key.php', true);
+
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4) {
             if (xhr.status === 200) {
-                let response = JSON.parse(xhr.responseText);
-                if (response.status === 'success') {
-                    console.log('regenerate_outline_manager_key executed successfully!', response.output);
-                    location.reload();
-                } else {
-                    console.error('regenerate_outline_manager_key error: ' + response.message);
+                try {
+                    let response = JSON.parse(xhr.responseText);
+                    if (response.status === 'success') {
+                        console.log('regenerate_outline_manager_key executed successfully!', response.output);
+                        location.reload();
+                    } else {
+                        console.error('regenerate_outline_manager_key error: ' + response.message);
+                    }
+                } catch (error) {
+                    console.error('Failed to parse response JSON:', error);
                 }
             } else {
                 console.error('Error: ' + xhr.statusText);
             }
         }
     };
+    
     xhr.send();
 }
--- outline/scripts/regenerate_outline_manager_key.php
+++ outline/scripts/regenerate_outline_manager_key.php
@@ -1,36 +1,23 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST')
+$script_path = './regenerate_outline_manager_key.sh';
+$output = [];
+$return_var = 0;
+
+exec("sh $script_path 2>&1", $output, $return_var);
+header('Content-Type: application/json; charset=utf-8');
+
+if ($return_var === 0)
 {
-	$script_path = './regenerate_outline_manager_key.sh';
-	$output = [];
-	$return_var = 0;
-
-	exec("sh $script_path 2>&1", $output, $return_var);
-	header('Content-Type: application/json; charset=utf-8');
-
-	if ($return_var === 0)
-	{
-		echo json_encode([
-			"status" => "success",
-			"message" => "Script executed successfully!",
-			"output" => $output
-		], JSON_PRETTY_PRINT);
-	}
-	else
-	{
-		$error_message = "[" . date('Y-m-d H:i:s') . "] Script execution error: " . implode("\n", $output) . "\n";
-		http_response_code(500);
-		echo json_encode([
-			"status" => "error",
-			"message" => $error_message,
-			"output" => ""
-		], JSON_PRETTY_PRINT);
-	}
+	echo json_encode([
+		"status" => "success",
+		"message" => "Script executed successfully!",
+		"output" => $output
+	], JSON_PRETTY_PRINT);
 }
 else
 {
-	$error_message = "[" . date('Y-m-d H:i:s') . "] Invalid request method.\n";
-	http_response_code(405);
+	$error_message = "[" . date('Y-m-d H:i:s') . "] Script execution error: " . implode("\n", $output) . "\n";
+	http_response_code(500);
 	echo json_encode([
 		"status" => "error",
 		"message" => $error_message,
