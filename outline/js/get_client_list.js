b3e25d481dbd7d625d720666168bd9e7434f9cad
alexeyralphs <138135367+alexeyralphs@users.noreply.github.com>
2025-02-12 02:03:27 +0300

    Update get_client_list.js

--- outline/js/get_client_list.js
+++ outline/js/get_client_list.js
@@ -1,4 +1,4 @@
-let client_list = [];
+let current_client_list = [];
 
 function populateClientsList() {
     const clientList = document.getElementById('client_list');
@@ -6,7 +6,7 @@ function populateClientsList() {
 
     clientList.innerHTML = ''; // Очищаем список перед обновлением
 
-    client_list.forEach(client => {
+    current_client_list.forEach(client => {
         const clone = document.importNode(template, true);
         clone.querySelector('.client-id').textContent = client.id;
         clone.querySelector('.client-name').textContent = client.name;
@@ -32,7 +32,7 @@ async function get_client_list() {
 
         const data = await response.json();
         if (data.status === 'success' && data.output && data.output.accessKeys) {
-            client_list = data.output.accessKeys; // Заполняем список клиентов
+            current_client_list = data.output.accessKeys; // Заполняем список клиентов
             populateClientsList(); // Обновляем DOM с клиентами
         } else {
             console.error("Неверный формат данных:", data);
@@ -42,5 +42,4 @@ async function get_client_list() {
     }
 }
 
-// Инициализация получения списка клиентов
 get_client_list();
