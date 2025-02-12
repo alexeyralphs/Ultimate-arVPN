a14a2652bd4cdad2039de6888335a2c8c746fe2a
alexeyralphs <138135367+alexeyralphs@users.noreply.github.com>
2025-02-12 03:59:40 +0300

    Update get_outline_manager_key.js

--- outline/js/get_outline_manager_key.js
+++ outline/js/get_outline_manager_key.js
@@ -1,8 +1,12 @@
 function get_outline_manager_key() {
     fetch('./scripts/get_outline_manager_key.php')
-        .then(response => response.text())
+        .then(response => response.json())
         .then(data => {
-            document.getElementById('outline_manager_key').textContent = data;
+            if (data && data.output) {
+                document.getElementById('outline_manager_key').textContent = data.output;
+            } else {
+                console.error('Error: Manager key is null!');
+            }
         })
         .catch(error => console.error('Error:', error));
 }
