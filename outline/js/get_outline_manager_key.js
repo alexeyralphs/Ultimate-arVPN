79c41553a4adf7222765f8d5f9965cf6dfeee579
alexeyralphs <138135367+alexeyralphs@users.noreply.github.com>
2025-02-12 03:20:31 +0300

    .

--- outline/js/get_outline_manager_key.js
+++ outline/js/get_outline_manager_key.js
@@ -1,3 +1,10 @@
+<?php
+	$keyFile = './outline_manager_key.php';
+	$keyContent = file_get_contents($keyFile);
+	$keyData = json_decode($keyContent, true);
+	?>
+    
+const outline_manager_key =  `<?php echo ($keyContent); ?>`;
 function get_outline_manager_key(current_outline_manager_key) {
     document.getElementById('outline_manager_key').textContent = current_outline_manager_key;
 }
