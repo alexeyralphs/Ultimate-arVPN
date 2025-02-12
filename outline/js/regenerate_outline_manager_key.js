4c1845d55dfe8c4469223661f327015a456cfa2e
alexeyralphs <138135367+alexeyralphs@users.noreply.github.com>
2025-02-12 04:03:07 +0300

    Update regenerate_outline_manager_key.js

--- outline/js/regenerate_outline_manager_key.js
+++ outline/js/regenerate_outline_manager_key.js
@@ -8,7 +8,7 @@ function regenerate_outline_manager_key() {
                 let response = JSON.parse(xhr.responseText);
                 if (response.status === 'success') {
                     console.log('regenerate_outline_manager_key executed successfully!', response.output);
-                    get_outline_manager_key(current_outline_manager_key);
+                    get_outline_manager_key();
                 } else {
                     console.error('regenerate_outline_manager_key error: ' + response.message);
                 }
