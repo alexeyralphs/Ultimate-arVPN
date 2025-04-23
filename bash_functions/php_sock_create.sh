f6dde3cd833f609514346b2f9be379a0e5a1e6b2
alexeyralphs <138135367+alexeyralphs@users.noreply.github.com>
2025-04-23 18:09:27 +0300

    Update php_sock_create.sh

--- bash_functions/php_sock_create.sh
+++ bash_functions/php_sock_create.sh
@@ -9,6 +9,7 @@ php_sock_create() {
     PHP_VERSION=$(sudo php -v | head -n 1 | grep "PHP" | awk '{print $2}' | cut -c 1-3)
 
     sudo curl -s -o /etc/php/$PHP_VERSION/fpm/pool.d/$admin_name.conf https://raw.githubusercontent.com/alexeyralphs/Ultimate-arVPN/refs/heads/main/php-fpm_vpnadmin.conf
+    sudo sed -i "s/\$admin_name/$admin_name/g" /etc/php/$PHP_VERSION/fpm/pool.d/$admin_name.conf
 
     sudo systemctl restart php$PHP_VERSION-fpm
 }
