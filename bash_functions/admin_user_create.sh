c9856c3bb58837df13febc25ba13e731a6e40b15
alexeyralphs <138135367+alexeyralphs@users.noreply.github.com>
2025-04-23 17:58:20 +0300

    Update admin_user_create.sh

--- bash_functions/admin_user_create.sh
+++ bash_functions/admin_user_create.sh
@@ -6,11 +6,11 @@ BLACK_FG=$(tput setaf 0)
 RESET=$(tput sgr0)
 
 admin_user_create() {
-    sudo htpasswd -c -b /etc/nginx/vpnadmin.htpasswd vpnadmin $PASSWORD
-    sudo useradd -m vpnadmin
-    echo "vpnadmin:$PASSWORD" | sudo chpasswd
-    usermod -aG sudo vpnadmin
+    sudo htpasswd -c -b /etc/nginx/$admin_name.htpasswd $admin_name $PASSWORD
+    sudo useradd -m $admin_name
+    echo "$admin_name:$PASSWORD" | sudo chpasswd
+    usermod -aG sudo $admin_name
 
-    sudo sed -i '/^vpnadmin /d' /etc/sudoers
-    echo "vpnadmin ALL=(ALL) NOPASSWD: /usr/bin/wget, /bin/bash, /bin/grep, /bin/rm, /usr/bin/tee, /bin/cat" | sudo tee -a /etc/sudoers
+    sudo sed -i '/^$admin_name /d' /etc/sudoers
+    echo "$admin_name ALL=(ALL) NOPASSWD: /usr/bin/wget, /bin/bash, /bin/grep, /bin/rm, /usr/bin/tee, /bin/cat" | sudo tee -a /etc/sudoers
 }
