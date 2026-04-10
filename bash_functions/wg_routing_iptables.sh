33145d6d993824f4fe55dc51e19e791087c7c239
alexeyralphs <138135367+alexeyralphs@users.noreply.github.com>
2026-04-11 00:43:12 +0300

    Update wg_routing_iptables.sh

--- bash_functions/wg_routing_iptables.sh
+++ bash_functions/wg_routing_iptables.sh
@@ -5,14 +5,16 @@ GREEN_BG=$(tput setab 42)
 BLACK_FG=$(tput setaf 0)
 RESET=$(tput sgr0)
 
-while iptables -D FORWARD -p tcp --dport 51821 -j DROP 2>/dev/null; do
-  echo "${BLUE_BG}${BLACK_FG}REMOVED iptables -I FORWARD -p tcp --dport 51821 -j DROP${RESET}"
-done
-iptables -I FORWARD -p tcp --dport 51821 -j DROP
-echo "${BLUE_BG}${BLACK_FG}APPLIED iptables -I FORWARD -p tcp --dport 51821 -j DROP${RESET}"
+wg_routing_iptables() {
+  while iptables -D FORWARD -p tcp --dport 51821 -j DROP 2>/dev/null; do
+    echo "${BLUE_BG}${BLACK_FG}REMOVED iptables -I FORWARD -p tcp --dport 51821 -j DROP${RESET}"
+  done
+  iptables -I FORWARD -p tcp --dport 51821 -j DROP
+  echo "${BLUE_BG}${BLACK_FG}APPLIED iptables -I FORWARD -p tcp --dport 51821 -j DROP${RESET}"
 
-while iptables -t mangle -D OUTPUT -m set --match-set vpn dst -j MARK --set-mark 1 2>/dev/null; do
-  echo "${BLUE_BG}${BLACK_FG}REMOVED iptables -t mangle -A OUTPUT -m set --match-set vpn dst -j MARK --set-mark 1${RESET}"
-done
-iptables -t mangle -A OUTPUT -m set --match-set vpn dst -j MARK --set-mark 1
-echo "${BLUE_BG}${BLACK_FG}APPLIED iptables -t mangle -A OUTPUT -m set --match-set vpn dst -j MARK --set-mark 1${RESET}"
+  while iptables -t mangle -D OUTPUT -m set --match-set vpn dst -j MARK --set-mark 1 2>/dev/null; do
+    echo "${BLUE_BG}${BLACK_FG}REMOVED iptables -t mangle -A OUTPUT -m set --match-set vpn dst -j MARK --set-mark 1${RESET}"
+  done
+  iptables -t mangle -A OUTPUT -m set --match-set vpn dst -j MARK --set-mark 1
+  echo "${BLUE_BG}${BLACK_FG}APPLIED iptables -t mangle -A OUTPUT -m set --match-set vpn dst -j MARK --set-mark 1${RESET}"
+}
